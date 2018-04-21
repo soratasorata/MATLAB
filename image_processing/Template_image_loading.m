b4e86ae3ea7bf98edd87553ab3ee316f670e29f8
sorattasoratta <sorattasoratta@gmail.com>
2018-04-21 13:12:32 +0200

    Add my_file_exist.m + my_imread.m

--- image_processing/Template_image_loading.m
+++ image_processing/Template_image_loading.m
@@ -3,11 +3,11 @@ clc; clearvars; close all;
 %% Load image
 source_path = 'Image/source/';
 result_path = 'Image/result/symetrie/';
-image       = 'img_01';
-% ext = '.png';
-ext = '.jpg';
+image       = 'img_08';
 
-img1 = imread([source_path image ext]);
+% [p, n, e] =  fileparts('Image/source/img_01')
+% img1 = imread([source_path image ext]);
+img = my_imread('Image/source/img_08');
 
 %% Treatement
-imshow(img1)
+imshow(img);
